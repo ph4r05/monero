f711e7f5983f0cb9495b491e1b09f2328bb02d27
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2018-01-05 12:36:32 +0000

    performance_tests: add (Borromean) range proofs

--- tests/performance_tests/main.cpp
+++ tests/performance_tests/main.cpp
@@ -51,6 +51,7 @@
 #include "sc_reduce32.h"
 #include "cn_fast_hash.h"
 #include "rct_mlsag.h"
+#include "range_proof.h"
 
 namespace po = boost::program_options;
 
@@ -149,6 +150,9 @@ int main(int argc, char** argv)
   TEST_PERFORMANCE3(filter, test_ringct_mlsag, 1, 5, true);
   TEST_PERFORMANCE3(filter, test_ringct_mlsag, 1, 10, true);
   TEST_PERFORMANCE3(filter, test_ringct_mlsag, 1, 100, true);
+
+  TEST_PERFORMANCE1(filter, test_range_proof, true);
+  TEST_PERFORMANCE1(filter, test_range_proof, false);
 
   std::cout << "Tests finished. Elapsed time: " << timer.elapsed_ms() / 1000 << " sec" << std::endl;
 
