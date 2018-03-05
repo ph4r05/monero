78aa494552fdb9daaa3e5beb43c09716bdadbd20
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-05 17:17:20 +0100

    simple serialization added

---
+++ src/simpleserialize/simpleserialize.cpp
@@ -0,0 +1,109 @@
+// Copyright (c) 2014-2018, The Monero Project
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//    of conditions and the following disclaimer in the documentation and/or other
+//    materials provided with the distribution.
+// 
+// 3. Neither the name of the copyright holder nor the names of its contributors may be
+//    used to endorse or promote products derived from this software without specific
+//    prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
+
+/*!
+ * \file simplewallet.cpp
+ * 
+ * \brief Source file that defines simple_wallet class.
+ */
+#include <thread>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <ctype.h>
+#include <boost/lexical_cast.hpp>
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
+#include <boost/regex.hpp>
+#include "include_base_utils.h"
+#include "common/i18n.h"
+#include "common/command_line.h"
+#include "common/util.h"
+#include "common/dns_utils.h"
+#include "common/base58.h"
+#include "common/scoped_message_writer.h"
+#include "cryptonote_protocol/cryptonote_protocol_handler.h"
+#include "simpleserialize.h"
+#include "cryptonote_basic/cryptonote_format_utils.h"
+#include "storages/http_abstract_invoke.h"
+#include "rpc/core_rpc_server_commands_defs.h"
+#include "crypto/crypto.h"  // for crypto::secret_key definition
+#include "mnemonics/electrum-words.h"
+#include "rapidjson/document.h"
+#include "common/json_util.h"
+#include "ringct/rctSigs.h"
+#include "multisig/multisig.h"
+#include "wallet/wallet_args.h"
+#include <stdexcept>
+
+#ifdef HAVE_READLINE
+#include "readline_buffer.h"
+#endif
+
+using namespace std;
+using namespace epee;
+using namespace cryptonote;
+using boost::lexical_cast;
+namespace po = boost::program_options;
+
+#undef MONERO_DEFAULT_LOG_CATEGORY
+#define MONERO_DEFAULT_LOG_CATEGORY "wallet.simpleserialize"
+
+namespace
+{
+  tools::scoped_message_writer success_msg_writer(bool color = false)
+  {
+    return tools::scoped_message_writer(color ? console_color_green : console_color_default, false, std::string(), el::Level::Info);
+  }
+
+  tools::scoped_message_writer message_writer(epee::console_colors color = epee::console_color_default, bool bright = false)
+  {
+    return tools::scoped_message_writer(color, bright);
+  }
+
+  tools::scoped_message_writer fail_msg_writer()
+  {
+    return tools::scoped_message_writer(console_color_red, true, "Error ", el::Level::Error);
+  }
+}
+
+
+
+//----------------------------------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+  std::ostringstream oss;
+  boost::archive::portable_binary_oarchive arx(oss);
+
+
+  std::cout << epee::string_tools::buff_to_hex_nodelimer(oss.str()) << std::endl;
+  std::cout << oss.str() << std::endl;
+}
