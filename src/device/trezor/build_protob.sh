34acda84e715062828f54133ccbef147b371f78f
Dusan Klinec <dusan.klinec@gmail.com>
2018-07-29 15:44:20 +0200

    device: trezor - message builder

--- src/device/trezor/build_protob.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-protoc external/trezor-common/protob/{messages.proto,messages-common.proto,messages-management.proto,messages-monero.proto} \
-    --cpp_out=src/device/trezor/messages/ \
-    --descriptor_set_out=src/device/trezor/descriptor \
-    -I external/trezor-common/protob/ -I /usr/local/include
---
+++ src/device/trezor/tools/README.md
@@ -0,0 +1,14 @@
+# Trezor
+
+## Python virtual env
+
+```bash
+python -m venv /
+```
+
+## Messages rebuild
+
+```
+./venv/bin/python src/device/trezor/tools/build_protob.py
+```
+
