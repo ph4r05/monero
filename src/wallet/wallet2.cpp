f00f54b2620a7b5fa94a2ade3828a7b789e84fc5
Dusan Klinec <dusan.klinec@gmail.com>
2018-08-09 18:54:17 +0200

    wallet: store device name to the wallet file

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -2837,6 +2837,9 @@ bool wallet2::store_keys(const std::string& keys_file_name, const epee::wipeable
   value2.SetUint(m_subaddress_lookahead_minor);
   json.AddMember("subaddress_lookahead_minor", value2, json.GetAllocator());
 
+  value2.SetString(m_device_name);
+  json.AddMember("device_name", value2, json.GetAllocator());
+
   // Serialize the JSON object
   rapidjson::StringBuffer buffer;
   rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
