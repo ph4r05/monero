2db401cfb7e4b3defefde6ed94b70f9db8951351
Dusan Klinec <dusan.klinec@gmail.com>
2018-10-23 22:31:12 +0200

    device/trezor: proper device disconnect on restore fail

--- src/cryptonote_basic/account.cpp
+++ src/cryptonote_basic/account.cpp
@@ -208,8 +208,13 @@ DISABLE_VS_WARNINGS(4244 4345)
     MCDEBUG("device", "device type: "<<typeid(hwdev).name());
     CHECK_AND_ASSERT_THROW_MES(hwdev.init(), "Device init failed");
     CHECK_AND_ASSERT_THROW_MES(hwdev.connect(), "Device connect failed");
-    CHECK_AND_ASSERT_THROW_MES(hwdev.get_public_address(m_keys.m_account_address), "Cannot get a device address");
-    CHECK_AND_ASSERT_THROW_MES(hwdev.get_secret_keys(m_keys.m_view_secret_key, m_keys.m_spend_secret_key), "Cannot get device secret");
+    try {
+      CHECK_AND_ASSERT_THROW_MES(hwdev.get_public_address(m_keys.m_account_address), "Cannot get a device address");
+      CHECK_AND_ASSERT_THROW_MES(hwdev.get_secret_keys(m_keys.m_view_secret_key, m_keys.m_spend_secret_key), "Cannot get device secret");
+    } catch (std::exception const& e){
+      hwdev.disconnect();
+      throw;
+    }
     struct tm timestamp = {0};
     timestamp.tm_year = 2014 - 1900;  // year 2014
     timestamp.tm_mon = 4 - 1;  // month april
