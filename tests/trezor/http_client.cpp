1faa30aa74800d94381df4063f15ad945b17bd62
Dusan Klinec <dusan.klinec@gmail.com>
2019-04-04 19:00:07 +0200

    ssl deadlock - modification which removes the bug

--- tests/trezor/http_client.cpp
+++ tests/trezor/http_client.cpp
@@ -56,15 +56,31 @@ using auth_responses = std::vector<fields>;
 
 }
 
+#define GET_VERSION_MC(m_http_client, VER) \
+  cryptonote::COMMAND_RPC_GET_VERSION::request req_t = AUTO_VAL_INIT(req_t);          \
+  cryptonote::COMMAND_RPC_GET_VERSION::response resp_t = AUTO_VAL_INIT(resp_t);       \
+  bool r = net_utils::invoke_http_json_rpc("/json_rpc", "get_version", req_t, resp_t, m_http_client, std::chrono::seconds(60)); \
+                                                                                      \
+  CHECK_AND_ASSERT_THROW_MES(r, "RPC error - Get version");                           \
+  CHECK_AND_ASSERT_THROW_MES(resp_t.status != CORE_RPC_STATUS_BUSY, "Daemon problem");\
+  CHECK_AND_ASSERT_THROW_MES(resp_t.status == CORE_RPC_STATUS_OK, "Daemon response inva lid: " << resp_t.status); \
+  VER = resp_t.version
+
 static uint64_t get_version(epee::net_utils::http::http_simple_client & m_http_client)
 {
+  uint64_t ver;
+  GET_VERSION_MC(m_http_client, ver);
+  return ver;
+}
+
+static uint64_t get_version2(epee::net_utils::http::http_simple_client & m_http_client){
   cryptonote::COMMAND_RPC_GET_VERSION::request req_t = AUTO_VAL_INIT(req_t);
   cryptonote::COMMAND_RPC_GET_VERSION::response resp_t = AUTO_VAL_INIT(resp_t);
   bool r = net_utils::invoke_http_json_rpc("/json_rpc", "get_version", req_t, resp_t, m_http_client, std::chrono::seconds(60));
 
   CHECK_AND_ASSERT_THROW_MES(r, "RPC error - Get version");
   CHECK_AND_ASSERT_THROW_MES(resp_t.status != CORE_RPC_STATUS_BUSY, "Daemon problem");
-  CHECK_AND_ASSERT_THROW_MES(resp_t.status == CORE_RPC_STATUS_OK, "Daemon response invalid: " << resp_t.status);
+  CHECK_AND_ASSERT_THROW_MES(resp_t.status == CORE_RPC_STATUS_OK, "Daemon response inva lid: " << resp_t.status);
   return resp_t.version;
 }
 
@@ -72,6 +88,7 @@ TEST(HTTP_Simple_Client, GetHeight)
 {
   const char * env_rpc_addr = getenv("RPC_ADDR");
   const char * env_force_ssl = getenv("FORCE_SSL");
+  const char * env_poc_method = getenv("POC_METHOD");
 
   std::string rpc_addr;
   if (!env_rpc_addr || !strlen(env_rpc_addr)){
@@ -82,6 +99,7 @@ TEST(HTTP_Simple_Client, GetHeight)
 
   bool use_custom_daemon = false;
   bool force_ssl = env_force_ssl && strlen(env_force_ssl) > 0 ? atoi(env_force_ssl) : 1;
+  int poc_method = env_poc_method && strlen(env_poc_method) > 0 ? atoi(env_poc_method) : 0;
 
   tools::on_startup();
   //epee::string_tools::set_module_name_and_folder("");
@@ -107,8 +125,21 @@ TEST(HTTP_Simple_Client, GetHeight)
   epee::net_utils::http::http_simple_client m_http_client;
   CHECK_AND_ASSERT_THROW_MES(m_http_client.set_server(rpc_addr, boost::none,
                                                       force_ssl ? epee::net_utils::ssl_support_t::e_ssl_support_enabled : epee::net_utils::ssl_support_t::e_ssl_support_autodetect), "HTTP client set failed");
+  uint64_t version = 0;
+  if (poc_method == 0){
+    MDEBUG("Method 0");
+    version = get_version(m_http_client);
 
-  MINFO("Version: " << get_version(m_http_client));
+  } else if (poc_method == 1){
+    MDEBUG("Method 1");
+    GET_VERSION_MC(m_http_client, version);
+  
+  } else if (poc_method == 2){
+    MDEBUG("Method 2");
+    version = get_version2(m_http_client);
+  }
+
+  MINFO("Version: " << version);
 
   if(m_http_client.is_connected())
     m_http_client.disconnect();
