0a72e0d72160a42c1c77ea4134b504395c9e06d1
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-06-04 14:07:52 +0000

    wallet: historical reserve proofs, and fix spent height on rescan

--- src/wallet/wallet_rpc_server.cpp
+++ src/wallet/wallet_rpc_server.cpp
@@ -2339,9 +2339,13 @@ namespace tools
       account_minreserve = std::make_pair(req.account_index, req.amount);
     }
 
+    boost::optional<uint64_t> blockchain_height;
+    if (req.blockchain_height != 0)
+      blockchain_height = req.blockchain_height;
+
     try
     {
-      res.signature = m_wallet->get_reserve_proof(account_minreserve, req.message);
+      res.signature = m_wallet->get_reserve_proof(account_minreserve, blockchain_height, req.message);
     }
     catch (const std::exception &e)
     {
@@ -2370,9 +2374,13 @@ namespace tools
       return false;
     }
 
+    boost::optional<uint64_t> blockchain_height;
+    if (req.blockchain_height != 0)
+      blockchain_height = req.blockchain_height;
+
     try
     {
-      res.good = m_wallet->check_reserve_proof(info.address, req.message, req.signature, res.total, res.spent);
+      res.good = m_wallet->check_reserve_proof(info.address, blockchain_height, req.message, req.signature, res.total, res.spent);
     }
     catch (const std::exception &e)
     {
